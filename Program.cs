3738422fa53cf61dfdb8e349e7817e52f664bc86
Kristian Ugahari Dwipa <48380624+KristianUgahari@users.noreply.github.com>
2019-06-25 14:55:20 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek List khusus untuk menampung tipe data string
+            List<string> lstNamaMhs = new List<string>();
+
+            //menambahkan elemen List
+            //tidak ada proses boxing
+            lstNamaMhs.Add("Kristian");
+            lstNamaMhs.Add("Ugahari");
+            lstNamaMhs.Add("Dwipa");
+
+            //mengakses elemen List
+            //tidak ada proses unboxing
+            string namaMhs = lstNamaMhs[1];
+
+            Console.WriteLine(namaMhs);
+            Console.WriteLine();
+
+            foreach (string nama in lstNamaMhs)
+            {
+                Console.WriteLine(nama);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
